41fe8c5e5f28eb95e0e29625622a11fe3b2ad018
ugochi <uakaluso@gmail.com>
2019-07-29 11:11:20 -0500

    updating machine learning.js and test.json

--- api/static/js/machineLearning.js
+++ api/static/js/machineLearning.js
@@ -1,97 +1,72 @@
 function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
   // Use `d3.json` to fetch the metadata for a sample 
-  const url = "../test.json"; //get the url for the API
+  const url = "../static/test.json"; //get the url for the API
   d3.json(url).then((sampleNames) => {
     // Use d3 to select the panel with id of `#sample-metadata`
     var sample_metadata = d3.select("#sample-metadata");
       // Clear existed data
       sample_metadata.html("");
       // Use `Object.entries` to add each key and value pair to the panel
-    //   Object.entries(sample).forEach(([key, value]) => {
-    //       console.log(key, value)
-    //     row = sample_metadata.append("p");
-    //     row.text(`${key} : ${value}`);
-    //   });
       Object.entries(sample).forEach(([key, value]) => {
-        console.log(key, value)
-      row = sample_metadata.append("p");
-      row.text(`${key} : ${value}`);
-        });
+
+        row = sample_metadata.append("p");
+        row.text(`${key} : ${value}`);
+      });
     });
 }
 
+
+
 // Build the chart 
-// function buildCharts(sample) {
-
-//   // Use d3.json to fetch the sample data for the plots
-//   const url = `samples/${sample}`;
-//   d3.json(url).then((data) => {
-//     // Build dynamic bubble plot data
-//     var x_value = data.otu_ids; //x-axis
-//     var y_value = data.sample_values; //y-axis
-//     var marker_color = data.otu_ids; // Marker color
-//     var marker_size = data.sample_values; // Marker size
-//     var text_value = data.otu_labels; // Text Value
-//     // Define the Bubble Plot
-//     var trace1 = {
-//       x: x_value,
-//       y: y_value,
-//       text: text_value,
-//       mode: 'markers',
-//       marker: {
-//         color: marker_color,
-//         size: marker_size,
-//         colorscale: "Picnic", 
-//       } 
-//     }
-    // Define the variable for building the plot
-//     var bubblePlotData = [trace1];
-
-//     var layout = {
-//       xaxis: { title: "OTU ID"},
-//       title: "OTU Volume and Spread",
-//       // Add my name at the top right of the bubble plot.. 
-//       annotations: [{
-//         text:'Hunter Cash',
-//         x: 1,
-//         y: 1,
-//         yref: 'paper',
-//         xref: 'paper',
-//         showarrow: false,
-//         font: {color:'red', size: 16}
-//       }],
-//     };
-//     // Show the graph on the ID of bubble in the html
-//     Plotly.newPlot('bubble', bubblePlotData, layout);
+function buildCharts(sample) {
    
-//     // Build a Pie Chart
-//     // Slice the Data for top 10
-//     d3.json(url).then((data) => {  
-//       var pieValue = data.sample_values.slice(0,10);
-//       var pieLabels = data.otu_ids.slice(0,10);
-//       var pieHover = data.otu_labels.slice(0,10);
-//       var pieData = [{
-//         values: pieValue,
-//         labels: pieLabels,
-//         hovertext: pieHover,
-//         type: 'pie',
-//       }];
-//       var pieLayout= {
-//         title: "Top Ten OTU",
-//       };
-//     // Show the graph on the ID of pie in the html
-//     Plotly.newPlot('pie', pieData, pieLayout);
-//     });
+    // Build a Line Chart
+
+    d3.json("../static/test.json").then((sample) => { 
+        var lineYValues = []
+      
+        sample.forEach(function(row){
+        //   row['Amount (in trillions)'] = +row['Amount (in trillions)']
+          lineYValues.push(row["Income 1 year after Graduation"])
+          lineYValues.push(row["Income 4 year after Graduation"])
+          lineYValues.push(row["Income 6 year after Graduation"])
+          lineYValues.push(row["Income 8 year after Graduation"])
+          lineYValues.push(row["Predicted Income"])
+        })
+        console.log(sample[0]) 
+      var lineXValues = [1,4,6,8,10];
+    //   var pieHover = data.otu_labels.slice(0,10);
+      var lineData = [{
+        x: lineXValues,
+        y: lineYValues,
+        // hovertext: pieHover,
+        type: 'line',
+      }];
+      var lineLayout= {
+        title: "Earnings After Years of Graduation ",
+        xaxis: {
+            title: "Years After Graduation"
+          },
+          yaxis: {
+            title: "Earnings"
+          }
+      };
+      console.log(lineYValues)
+      console.log(lineXValues)
+
+    // Show the graph on the ID of pie in the html
+    Plotly.newPlot('line', lineData, lineLayout);
+    });
 //   });   
-// };
+};
 
 // function to create the dropdown menu for the names
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
   // Use the list of sample names to populate the select options
-  d3.json("../test.json").then((sampleNames) => {
+  d3.json("../static/test.json").then((sampleNames) => {
     sampleNames.forEach((sample) => {
 
       selector
@@ -102,8 +77,9 @@ function init() {
 
     // Use the first sample from the list to build the initial plots
     const firstSample = sampleNames[0];
-    // // buildCharts(firstSample);
+    buildCharts(firstSample);
     buildMetadata(firstSample);
+
   });
 }
 
@@ -116,3 +92,44 @@ function init() {
 
 // Initialize the dashboard
 init();
+
+//table for additional school info
+
+// d3.json('data.json', function (error,data) {
+
+//     function tabulate(data, columns) {
+//           var table = d3.select('body').append('table')
+//           var thead = table.append('thead')
+//           var	tbody = table.append('tbody');
+  
+//           // append the header row
+//           thead.append('tr')
+//             .selectAll('th')
+//             .data(columns).enter()
+//             .append('th')
+//               .text(function (column) { return column; });
+  
+//           // create a row for each object in the data
+//           var rows = tbody.selectAll('tr')
+//             .data(data)
+//             .enter()
+//             .append('tr');
+  
+//           // create a cell in each row for each column
+//           var cells = rows.selectAll('td')
+//             .data(function (row) {
+//               return columns.map(function (column) {
+//                 return {column: column, value: row[column]};
+//               });
+//             })
+//             .enter()
+//             .append('td')
+//               .text(function (d) { return d.value; });
+  
+//         return table;
+//       }
+  
+//       // render the table(s)
+//       tabulate(data, ['date', 'close']); // 2 column table
+  
+//   });
