cc8cbd84d2bac7779b00f181c7d9ee6426773f2f
ugochi <uakaluso@gmail.com>
2019-07-30 01:00:35 -0500

    updating machineLearning js

--- api/static/js/machineLearning.js
+++ api/static/js/machineLearning.js
@@ -1,9 +1,9 @@
 function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
   // Use `d3.json` to fetch the metadata for a sample 
-  const url = "../static/test.json"; //get the url for the API
-  d3.json(url).then((sampleNames) => {
-    console.log(sample)
+  const url = "/api/wiki/100654"; //get the url for the API
+  d3.json(url).then((sample) => {
+    // console.log(sample)
     // Use d3 to select the panel with id of `#sample-metadata`
     var sample_metadata = d3.select("#sample-metadata");
       // Clear existed data
@@ -16,11 +16,9 @@ function buildMetadata(sample) {
     });
 }
 
-function buildcard(sample) {
-  // @TODO: Complete the following function that builds the metadata panel
-  // Use `d3.json` to fetch the metadata for a sample 
-  const url = "../static/test.json"; //get the url for the API
-  d3.json(url).then((sampleNames) => {
+function buildCard(sample) {
+ 
+  d3.json("/api/wiki/100654").then((sample) => {
     // Use d3 to select the panel with id of `#sample-metadata`
     var sample_summary = d3.select("#sample-summary");
       // Clear existed data
@@ -29,29 +27,45 @@ function buildcard(sample) {
            var row = sample_summary
            
             Object.entries(sample).forEach(([key, value]) => {
-              
-               if ( key == "institution_name") {
-                row.append("th").text(`${value}`);
+
+              if (key =="ADM_RATE_ALL") {
+                row.append("p").text(`Admission Rate: ${value}`);
                }
-               if (key =="Established") {
-                row.append("tr").text(`${key}: ${value}`);
+               if (key =="NICKNAME") {
+                row.append("p").text(`Nickname: ${value}`);
                }
-               if (key =="Nickname") {
-                row.append("tr").text(`${key}: ${value}`);
+               if (key =="MOTTO") {
+                row.append("p").text(`Motto: ${value}`);
                }
-               if (key =="Motto") {
-                row.append("tr").text(`${key}: ${value}`);
+               if (key =="AGE_ENTRY") {
+                row.append("p").text(`Age of Entry: ${value}`);
                }
-              //  if (key == "Logo_URL") {
-                // row.append(`""item <img src=" + ${value} + "/>""`)
-              //  }
-              if (key =="Undergraduate") {
-                row.append("tr").text(`${key}: ${value}`);
+               if (key =="YEAR_EST") {
+                row.append("p").text(`Established: ${value}`);
+               }
+               if (key =="CAMPUS") {
+                row.append("p").text(`Campus: ${value}`);
+               }
+               if (key =="GRAD_DEBT_MDN") {
+                row.append("p").text(`Graduating Median Debt: ${value}`);
+               }
+               if (key =="CONTROL") {
+                row.append("p").text(`Type: ${value}`);
+               }
+               if (key =="SAT_AVG_ALL") {
+                row.append("p").text(`Average SAT Score: ${value}`);
+               }
+               if (key =="STUDENT_POP") {
+                row.append("p").text(`Student Population: ${value}`);
+               }
+               if (key =="TUITIONFEE_IN") {
+                row.append("p").text(`Tuition Fee IN: ${value}`);
+               }
+               if (key =="TUITIONFEE_OUT") {
+                row.append("p").text(`Tuition Fee Out: ${value}`);
                }
 
             });
-
-            // row.append("td").text(value));
     });
 }
 
@@ -59,21 +73,17 @@ function buildcard(sample) {
 
 // Build the chart 
 function buildCharts(sample) {
-   
     // Build a Line Chart
-
-    d3.json("../static/test.json").then((sample) => { 
+    d3.json("/api/wiki/100654").then((sample) => { 
+      console.log(sample)
         var lineYValues = []
       
-        sample.forEach(function(row){
-          lineYValues.push(row["Income 1 year after Graduation"])
-          lineYValues.push(row["Income 4 year after Graduation"])
-          lineYValues.push(row["Income 6 year after Graduation"])
-          lineYValues.push(row["Income 8 year after Graduation"])
-          lineYValues.push(row["Predicted Income"])
-        }) 
+        // sample.forEach(function(row){
+          lineYValues.push(sample["MEAN_EARN_6"])
+          lineYValues.push(sample["MEAN_EARN_8"])
+        // }) 
 
-      var lineXValues = [1,4,6,8,10];
+      var lineXValues = [6,8];
       var lineData = [{
         x: lineXValues,
         y: lineYValues,
@@ -102,30 +112,61 @@ function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
   // Use the list of sample names to populate the select options
-  d3.json("../static/test.json").then((sampleNames) => {
+  d3.json("/api/id_and_name.json").then((sampleNames) => {
     sampleNames.forEach((sample) => {
 
       selector
         .append("option")
-        .text(sample.institution_name)
-        .property("value", sample.institution_name);
+        .text(sample.INSTNM)
+        .property("value", sample.UNITID);
     });
-
-    // Use the first sample from the list to build the initial plots
-    const firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-    buildcard(firstSample);
   });
 }
 
+
 // Function to create the new chart on change
-// function optionChanged(newSample) {
-//   // Fetch new data each time a new sample is selected
-//   buildCharts(newSample);
-//   buildMetadata(newSample);
-// }
+function optionChanged(sample) {
+  var unit= sample
+  console.log(unit)
+  // Fetch new data each time a new sample is selected
+  buildCharts(sample);
+  buildMetadata(sample);
+}
+
+function buildSummary(sample) {
+ console.log(sample)
+  const url=`/api/wiki/100654`;
+  // console.log(url)
+  
+  d3.json(url).then((sample) => {
+    console.log(sample)
+    // Use d3 to select the panel with id of `#sample-metadata`
+    var sample_summary = d3.select("#summary");
+      // Clear existed data
+      sample_summary.html("");
+    
+           var row = sample_summary
+           
+            Object.entries(sample).forEach(([key, value]) => {
+              
+               if ( key == "INSTNM") {
+                row.append("h3").text(`${value}`);
+               }
+ 
+              if (key =="SNIPPET") {
+                row.append("p").text(`${value}`);
+               }
+
+            });
+
+            // row.append("td").text(value));
+    });
+}
 
 // Initialize the dashboard
 init();
+// buildMetadata();
+buildCharts();
+buildSummary();
+buildCard();
 
